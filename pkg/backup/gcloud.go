461c571e35d171eed797f746a7f506c7dac60bc4
Lam Huynh <thanhlam0290@gmail.com>
2022-03-21 22:41:26 +0700

    Update gcloud.go

--- pkg/backup/gcloud.go
+++ pkg/backup/gcloud.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"time"
+	"path/filepath"
 
 	"github.com/codeskyblue/go-sh"
 	"github.com/pkg/errors"
@@ -22,18 +23,21 @@ func gCloudUpload(file string, plan config.Plan) (string, error) {
 	}
 
 	upload := fmt.Sprintf("gsutil cp %v gs://%v/%v",
-		file, plan.GCloud.Bucket, file)
-
+		file, plan.GCloud.Bucket, filepath.Base(file))
+	
 	result, err := sh.Command("/bin/sh", "-c", upload).SetTimeout(time.Duration(plan.Scheduler.Timeout) * time.Minute).CombinedOutput()
 	output := ""
 	if len(result) > 0 {
 		output = strings.Replace(string(result), "\n", " ", -1)
 	}
-
+		
 	if err != nil {
 		return "", errors.Wrapf(err, "GCloud uploading %v to gs://%v failed %v", file, plan.GCloud.Bucket, output)
 	}
-
+		
+	cleanoldfiles := fmt.Sprintf("gsutil ls -l gs://%v | sort -k 2 -r | awk 'NR>20' | awk {'if($3) print $3'} | xargs gsutil rm", plan.GCloud.Bucket)
+	sh.Command("/bin/sh", "-c", cleanoldfiles).SetTimeout(time.Duration(plan.Scheduler.Timeout) * time.Minute).CombinedOutput()
+	
 	if strings.Contains(output, "<ERROR>") {
 		return "", errors.Errorf("GCloud upload failed %v", output)
 	}
